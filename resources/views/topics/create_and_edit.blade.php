d5925f288e3465d6a9da464038f7a0fdbf788a90
shijf <shijf_private.com>
2017-12-02 01:34:44 +0000

    WYSIWYG Editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
+@section('style')
+    <link rel="stylesheet" href="{{asset('css/simditor.css')}}">
+@endsection
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
 
+    <script>
+    $(document).ready(function(){
+        var editor = new Simditor({
+            textarea: $('#editor'),
+        });
+    });
+    </script>
+@endsection
 @section('content')
 
 <div class="container">
