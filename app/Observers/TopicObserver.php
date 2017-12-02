63f430f59c20e8b53206724e920f4fc1e51a01f8
shijf <shijf_private.com>
2017-12-02 02:19:33 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -15,6 +15,13 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
     }
 
+    public function saving(Topic $topic)
+    {
+        $topic->body = clean($topic->body, 'user_topic_body');
+
+        $topic->excerpt = make_excerpt($topic->body);
+    }
+
     public function updating(Topic $topic)
     {
         //
